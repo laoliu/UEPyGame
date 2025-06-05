3e79eaed584349e9ab3ba4511e703290b2081a1f
liu <imonlyyou2012@sina.com>
2025-06-05 10:12:46 +0800

    initial black project

---
+++ Source/UEPyGame/UEPyGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEPyGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEPyGame, "UEPyGame" );
